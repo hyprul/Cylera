c6b49441dc9b0e9b73471861435a944fafd15a68
Sean Huang <hyprul@ucla.edu>
2019-11-19 15:42:28 -0500

    backend done

--- backend/devices.py
+++ backend/devices.py
@@ -69,5 +69,5 @@ def read():
     return [DEVICES_HASH[key] for key in DEVICES_HASH.keys()]
 
 
-def getdevice(deviceid):
-    return DEVICES_HASH[deviceid]
+def getdevice(deviceId):
+    return DEVICES_HASH[deviceId]
